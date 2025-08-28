6395d58703f27e4bd46daecbd0bd572dbcff363a
Levente Bognar <lajos93@users.noreply.github.com>
2025-08-28 12:20:21 +0200

    add testupload

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import tilesRouter from "./routes/tiles";
 import generateTilesRouter from "./routes/generateTiles";
+import testUploadRouter from "./routes/testUpload";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,6 +11,8 @@ app.use("/tiles", tilesRouter);
 
 // /admin/
 app.use("/admin", generateTilesRouter);
+
+app.use("/admin", testUploadRouter);
 
 app.listen(PORT, () => {
   console.log(`Tile server running on port ${PORT}`);
